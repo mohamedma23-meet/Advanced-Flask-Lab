6a822f1eaccfde90d122eb9679fa941c7a811c20
malaktatour <109899496+mohamedma23-meet@users.noreply.github.com>
2022-07-27 23:01:50 +0300

    done

--- Advanced-Flask-Lab/app.py
+++ Advanced-Flask-Lab/app.py
@@ -25,8 +25,12 @@ posts = {
 
 @app.route('/')  # '/' for the default page
 def home():
-    return render_template('index.html')
+    return render_template('index.html',user_bio = user_bio,
+        image_link = image_link, posts=posts)
 
+# @app.route('/about')  # '/' for the default page
+#     return render_template('index.html',user_bio = "Middle East Entrepreneurs of Tomorrow. Enabling the next generation of Israeli and Palestinian leaders.",
+#         image_link = "https://scontent.ftlv21-1.fna.fbcdn.net/v/t31.18172-1/1502644_10152506946962507_2442851309927378964_o.png?stp=c49.0.148.148a_dst-png_p148x148&_nc_cat=111&ccb=1-7&_nc_sid=1eb0c7&_nc_ohc=d-R_wjIm2d8AX_NugPv&_nc_ht=scontent.ftlv21-1.fna&oh=00_AT8CSjiG8-biys_qQ9tU-yrxzJSp6HANxsz5c3mFVk-G2Q&oe=62FD0A1B",posts=posts)
 
 @app.route('/about')  # '/' for the default page
 def about():
